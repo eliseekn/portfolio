1af719bfa9ee7f0e6e948ec516da458693d18da6
eliseekn <eliseekn@gmail.com>
2020-02-08 19:43:55 +0000

    Update

--- assets/js/scroll-top.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 180) {
-            $("#scroll-top").fadeIn('slow');
-        } else {
-            $("#scroll-top").fadeOut('slow');
-        }
-    });
-});
