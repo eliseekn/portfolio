428a887b100facc8a5406deeb5cdebfc8ec5465d
eliseekn <eliseekn@gmail.com>
2020-07-18 10:58:20 +0000

    feat: contact form
    added contact form that use google sheet to save contact infos

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
         once: true
     })
 
-    $(".owl-carousel").owlCarousel({
+    $("#services .owl-carousel").owlCarousel({
+        items: 2,
+        margin: 15,
+        mouseDrag: false,
+        touchDrag: true,
+        loop: false,
+        nav: false,
+        responsive : {
+            0: {
+                items: 1
+            },
+
+            768: {
+                items: 2
+            }
+        }
+    })
+
+    $("#portfolio .owl-carousel").owlCarousel({
         items: 1,
         mouseDrag: false,
-        touchDrag: false,
+        touchDrag: true,
+        loop: false,
+        nav: false
+    })
+
+    $("#projects .owl-carousel").owlCarousel({
+        items: 1,
+        mouseDrag: false,
+        touchDrag: true,
         loop: false,
         nav: false
     })
@@ -22,17 +48,39 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#modal').setAttribute('style', 'display: none');
     })
 
+    document.querySelector('#modal').addEventListener('click', () => {
+        document.querySelector('#modal').setAttribute('style', 'display: none');
+    })
+
     window.addEventListener('scroll', () => {
-        if (document.querySelector('#scroll-top')) {
-            if (window.scrollY > 180) {
-                $('#scroll-top').fadeIn('slow')
-            } else {
-                $('#scroll-top').fadeOut('slow')
-            }
+        if (window.scrollY > 280) {
+            $('#scroll-top').fadeIn('slow')
+        } else {
+            $('#scroll-top').fadeOut('slow')
         }
     })
-})
 
-function closeLightbox() {
-    
-}
+    document.querySelector('#contact-form').addEventListener('submit', event => {
+        event.preventDefault()
+
+        let object = {}
+        const formData = new FormData(document.querySelector('#contact-form'))
+        formData.forEach((value, key) => {object[key] = value})
+        
+        fetch('https://script.google.com/macros/s/AKfycbx8VMQXTGdaDeTDb6_L8kRv2-eRaQ5D2Fq2RoevQH6EHYfPU94l/exec', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(object)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.result === 'success') {
+                alert('Votre message a bien été envoyé. Vous serez recontactez dans les plus brefs délais.')
+            } else {
+                alert('Une erreur est survenue lors du traitement de votre message. Si le problème persiste, je vous suggère d\'utiliser directement email indiquée sur la page.')
+            }
+
+            document.querySelector('#contact-form').reset()
+        })
+    })
+})
