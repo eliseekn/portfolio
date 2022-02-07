b79a629a647f2604479072718b743e38b753c1ff
N'Guessan Kouadio Elisée <eliseekn@gmail.com>
2022-02-07 01:14:46 +0000

    feat: added more works

--- src/data/work-data.ts
+++ src/data/work-data.ts
@@ -5,6 +5,8 @@ import iziplanning from '../../img/work/iziplanning.png'
 import diaspozen from '../../img/work/diaspozen.png'
 import yellior from '../../img/work/yellior.png'
 import cabinet_gesma from '../../img/work/cabinet_gesma.png'
+import eliseekn from '../../img/work/eliseekn.png'
+import immobiliere_abidjan from '../../img/work/immobiliere_abidjan.png'
 
 export interface IData {
     url: string,
@@ -17,13 +19,31 @@ export interface IData {
 export const workData: Array<Array<IData>> = [
     [
         {
+            url: 'https://immobiliere-abidjan.vercel.app/',
+            img: immobiliere_abidjan,
+            alt: 'Immobilière d\'Abidjan',
+            skills: 'Next.js, Bootstrap',
+            desc: 'Design et intégration de la maquette, développement de l\'application web'
+        },
+
+        {
+            url: '#',
+            img: eliseekn,
+            alt: 'Portfolio | Elisée Kouadio N\'Guessan',
+            skills: 'React.js, TailwindCSS',
+            desc: 'Design et intégration de la maquette'
+        },
+
+        {
             url: 'https://cps-securite.com',
             img: cps_securite,
             alt: 'CPS Sécurité',
             skills: 'Laravel',
-            desc: 'Intégration backend du site'
-        },
-
+            desc: 'Implémentation backend du site'
+        }
+    ],
+    
+    [
         {
             url: 'https://iziloyer.net',
             img: iziloyer,
@@ -38,23 +58,23 @@ export const workData: Array<Array<IData>> = [
             alt: 'Les Ateliers Nappy',
             skills: 'WordPress, API EtsyShop',
             desc: 'Refonte et personnalisation du thème'
-        }
-    ],
-    
-    [
+        },
+
         {
             url: 'https://iziplanning.com',
             img: iziplanning,
             alt: 'IZI PLANNING',
             skills: 'Laravel, Bootstrap, Javascript',
             desc: 'Intégration de la maquette, développement de l\'application web'
-        },
-
+        }
+    ],
+    
+    [
         {
             url: 'https://diaspozen.com',
             img: diaspozen,
             alt: 'DIASPO ZEN',
-            skills: 'TinyMVC, Bootstrap, Javascript',
+            skills: 'Laravel, TinyMVC, Bootstrap, Javascript',
             desc: 'Intégration de la maquette, développement de l\'application web'
         },
 
@@ -62,17 +82,15 @@ export const workData: Array<Array<IData>> = [
             url: 'https://yellior.ci',
             img: yellior,
             alt: 'Yellior',
-            skills: 'GIMP, TinyMVC, Bootstrap, Javascript',
+            skills: 'TinyMVC, Bootstrap, Javascript',
             desc: 'Design et intégration de la maquette, développement de l\'application web'
-        }
-    ],
-    
-    [
+        },
+
         {
             url: 'https://cabinetgesma.com',
             img: cabinet_gesma,
             alt: 'Cabinet GESMA',
-            skills: 'GIMP, WordPress',
+            skills: 'WordPress, Bootstrap',
             desc: 'Design et intégration de la maquette'
         },
     ]
