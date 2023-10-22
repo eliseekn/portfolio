c88e7d95f49296d5768c35a9e2c4b268f3f3dfb2
eliseekn <eliseekn@gmail.com>
2023-10-22 19:36:46 +0000

    Update UI

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,36 +1,48 @@
 <script setup lang="ts">
-    import avatar from '../../img/avatar.png'
+import avatar from "../../img/avatar.png";
 
-    const props = defineProps<{ setActivePage: (page: string) => void }>()
+const props = defineProps<{ setActivePage: (page: string) => void }>();
 </script>
 
 <template>
-    <div class="flex items-center min-h-screen">
-        <div class="grid grid-cols-2 gap-3">
-            <div class="col-span-6 md:col-auto my-auto order-2 md:order-1 text-center md:text-left">
-                <p>
-                    <span class="text-base md:text-xl md:leading-loose">Hello 👋, I am</span> <br>
-                    <span class="text-lg md:text-3xl md:leading-loose font-semibold">Elisée K. N'GUESSAN,</span>
-                </p>
+  <div class="flex items-center min-h-screen">
+    <div class="grid grid-cols-2 gap-3">
+      <div
+        class="col-span-6 md:col-auto my-auto order-2 md:order-1 text-center md:text-left"
+      >
+        <p>
+          <span class="text-base md:text-xl md:leading-loose"
+            >Hello 👋, I am</span
+          >
+          <br />
+          <span class="text-lg md:text-3xl md:leading-loose font-semibold"
+            >Elisée K. N'GUESSAN,</span
+          >
+        </p>
 
-                <h1 class="my-6 text-[#7e97a6] uppercase font-black text-3xl md:text-5xl">
-                    Freelance Developer.
-                </h1>
+        <h1
+          class="my-6 text-[#7e97a6] uppercase font-black text-3xl md:text-5xl"
+        >
+          Freelance Developer.
+        </h1>
 
-                <p class="text-base md:text-xl md:leading-loose">
-                    Self-taught and passionate about computer programming, I have 4+ years of experience in web and mobile application development.
-                </p>
+        <p class="text-base md:text-xl md:leading-loose">
+          Self-taught and passionate about computer programming, I have 4+ years
+          of experience in web application development.
+        </p>
 
-                <div class="animate-slide-in flex items-center justify-center md:justify-start mt-10 xl:hidden">
-                    <button class="btn" @click="props.setActivePage('Services')">
-                        Services
-                    </button>
-                </div>
-            </div>
-
-            <div class="col-span-6 md:col-auto m-auto order-1 md:order-2">
-                <img :src=avatar alt="Avatar" loading="lazy" class="w-28 md:w-full" />
-            </div>
+        <div
+          class="animate-slide-in flex items-center justify-center md:justify-start mt-10 xl:hidden"
+        >
+          <button class="btn" @click="props.setActivePage('Services')">
+            Services
+          </button>
         </div>
+      </div>
+
+      <div class="col-span-6 md:col-auto m-auto order-1 md:order-2">
+        <img :src="avatar" alt="Avatar" loading="lazy" class="w-28 md:w-full" />
+      </div>
     </div>
+  </div>
 </template>
